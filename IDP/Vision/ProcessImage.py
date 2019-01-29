456a55c6480fcc343f166f1e291b86768b9a0956
AungPaingJiJi <43409309+AungPaingJiJi@users.noreply.github.com>
2019-01-29 08:36:27 +0630

    updated two functions (region_of_interest and warp)
    The default value for the boundary , source and destination point need some advice

--- IDP/Vision/ProcessImage.py
+++ IDP/Vision/ProcessImage.py
@@ -5,4 +5,20 @@ import cv2
 # ProcessImage Class for preprocessing images
 # using computer vision techniques
 class ProcessImage(object):
-    pass
+    
+    '''return a crop image for the region_of_interest   
+    Boundry value will need to be decided by the user
+    '''    
+    def region_of_interest(img,boundry=np.array([[]])):
+        mask = np.zeros_like(img)
+        cv2.fillPoly(mask,boundry,255)
+        masked_image = cv2.bitwise_and(mask,img)
+        return masked_image
+    
+    '''return a warp(perspective change view of the image)
+    The src and dest should be in the format of np.float32()'''
+    def warp(img ,src,dest):
+        M = cv2.getPerspectiveTransform(src,dest)
+        warped= cv2.warpPerspective(img,M,(img.shape[1],img.shape[0]))
+        return warped
+
